f6ad27e45346f0f02b09fa38c9ce57f32aea8f1c
Guilherme Messias <142668009+messiasg@users.noreply.github.com>
2023-10-08 08:52:19 -0300

    loop
    Utilize um loop for para gerar a tabuada de um número escolhido pelo usuário.

---
+++ tabuada.js
@@ -0,0 +1,5 @@
+let numeroEscolhaUsuario = parseFloat(prompt("Digite um número:"))
+
+for (let multiplicacao = 1; multiplicacao <= 10; multiplicacao++) {
+    alert(numeroEscolhaUsuario + " X " + multiplicacao + " = " + numeroEscolhaUsuario*multiplicacao)
+}
