132e212a11787510ebf9ded285529e1881d8c713
Guilherme Messias <142668009+messiasg@users.noreply.github.com>
2023-11-06 05:29:22 -0300

    function e array

---
+++ MaiorEMenorNumero.js
@@ -0,0 +1,15 @@
+// Maior número
+
+function MaiorNumero (num1, num2, num3) {
+ return Math.max(num1, num2, num3);
+}
+
+console.log(MaiorNumero(2659,353,44))
+
+// Menor Número
+
+function menorNumero(numeros) {
+    return Math.min(...numeros);
+}
+
+console.log(menorNumero([945, 5, 7, 9, 12461]));
--- maiorNumero.js
+++
@@ -1,5 +0,0 @@
-function MaiorNumero (num1, num2, num3) {
- return Math.max(num1, num2, num3);
-}
-
-console.log(MaiorNumero(2659,353,44))
