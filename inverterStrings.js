0a6857d131b277972f63033460fb7d53833c8fd4
Guilherme Messias <142668009+messiasg@users.noreply.github.com>
2023-11-22 04:55:00 -0300

    for function

--- inverterStrings.js
+++ inverterStrings.js
@@ -10,3 +10,20 @@ function inverterOrdem(stringDigitada) {
 
 const resultado = inverterOrdem("Gato")
 console.log(resultado)
+
+/////////////////////////////////////////////////
+
+function comparacaoDeStrings() {
+  let palavra = "corredor";
+  let palavraInversa = "";
+  for (i = palavra.length - 1; i >= 0; i--) {
+    palavraInversa += palavra[i];
+  }
+  if (palavra == palavraInversa) {
+    return console.log("Palavra inversa: " + palavraInversa + "\nEstão a resposta é Sim");
+  } else {
+    return console.log("Palavra inversa: " + palavraInversa + "\nEstão a resposta é Não");
+  }
+}
+
+comparacaoDeStrings()
